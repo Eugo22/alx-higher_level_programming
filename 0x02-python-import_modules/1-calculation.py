d5c8c22aad47c9c8c93d395e87ce122e1defa823
Eugo22 <eugenecodez3@gmail.com>
2023-10-05 15:59:07 -0700

    a program that imports functions from the file calculator_1.py, does some Maths

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,33 +2,19 @@
 
 
 if __name__ == "__main__":
-    from  calculator_1 import add
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
 
+    # Printing the result of addition
     print("{} + {} = {}".format(a, b, add(a, b)))
 
-if __name__ == "__main__":
-    from calculator_1 import sub
-
-    a = 10
-    b = 5
-
+    # Printing the result of subtraction
     print("{} - {} = {}".format(a, b, sub(a, b)))
 
-if __name__ == "__main__":
-    from calculator_1 import mul
-
-    a = 10
-    b = 5
-
+    # Printing the result of multiplication
     print("{} * {} = {}".format(a, b, mul(a, b)))
 
-if __name__ == "__main__":
-    from calculator_1 import div
-
-    a = 10
-    b = 5
-
+    # Printing the result of division
     print("{} / {} = {}".format(a, b, div(a, b)))
