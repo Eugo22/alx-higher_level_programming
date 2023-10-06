a4c2fd075115899b0484b85243aa53a757b7a1c9
Eugo22 <eugenecodez3@gmail.com>
2023-10-06 11:21:12 -0700

    update on 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,12 @@ import sys
 
 if __name__ == "__main__":
 
-    num_args = len(sys.argv) -1
+    num_args = len(sys.argv) - 1
 
     """Print the number of and list of arguments."""
 
-    start = 's' if num-args != 1 else ''
-    ending = '.' if num_args == 0 esle ':'
+    start = 's' if num_args != 1 else ''
+    ending = '.' if num_args == 0 else ':'
     print("{} argument{}{}".format(num_args, start, ending))
-    for i in range(count):
+    for i in range(num_args):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
