6a53e0dd7b9268bff30835a0e13e78037c0ae648
Eugo22 <eugenecodez3@gmail.com>
2023-10-06 12:22:44 -0700

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
+# Check if this script is being run as the main program
 if __name__ == "__main__":
-
     import hidden_4
 
     names = dir(hidden_4)
+
     for name in names:
+
         if not name.startswith("__"):
-              print(name)
+
+            print(name)
